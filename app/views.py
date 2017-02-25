2536f0daf2f969465c7307b47af0d16fa833147f
vidalCreates <amc12vidal@hotmail.com>
2017-02-24 21:07:57 -0500

    corrected step 9 (redirect to login)

--- app/views.py
+++ app/views.py
@@ -11,6 +11,8 @@ from flask_login import login_user, logout_user, current_user, login_required
 from forms import LoginForm
 from models import UserProfile
 
+login_manager.login_view = "login"
+login_manager.login_message = "NB: Restricted access. Please login to access this page."
 
 ###
 # Routing for your application.
@@ -32,24 +34,26 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        print "test1"
         if form.validate_on_submit():
             # Get the username and password values from the form.
             username = form.username.data
             password = form.password.data
-            print "test2"
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
             user = UserProfile.query.filter_by(username=username, password=password).first()
 
-            # get user id, load into session
-            login_user(user)
+            if user != None:
+                # get user id, load into session
+                login_user(user)
+                # remember to flash a message to the user
+                flash('Logged in successfully.','success')
+                return redirect(url_for("secure_page"))# they should be redirected to a secure-page route instead
+            else:
+                flash('Logged in failed.','danger')
+                return redirect(url_for("login"))
 
-            # remember to flash a message to the user
-            flash('Logged in successfully.', 'success')
-            return redirect(url_for("secure_page"))# they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 @app.route("/secure-page")
